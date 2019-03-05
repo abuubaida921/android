918bc565ad69029d704699b9f7cda9e57343e7a8
Abu Ubaida <ubaida_nazia@yahoo.com>
2019-03-05 15:03:58 +0600

    New Version

--- app/src/main/java/com/rurinausoft/android/MainActivity.java
+++ app/src/main/java/com/rurinausoft/android/MainActivity.java
@@ -2,13 +2,19 @@ package com.rurinausoft.android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    RelativeLayout l;
+    TextView t;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        t = findViewById(R.id.Hello);
     }
 }
