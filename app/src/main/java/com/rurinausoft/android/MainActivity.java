ce04ea1779a8e8dfe090ec2b96748cf0cc75b249
abuubaida921 <ubaida_nazia@yahoo.com>
2019-03-05 14:32:23 +0600

    New Update

--- app/src/main/java/com/rurinausoft/android/MainActivity.java
+++ app/src/main/java/com/rurinausoft/android/MainActivity.java
@@ -2,12 +2,9 @@ package com.rurinausoft.android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ListView;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView l;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
